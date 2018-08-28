94ae7ec7a2fed30236605a37349c0538cb8b7f42
Andrzej <ganczarek.a@gmil.com>
2018-08-28 18:35:47 +0200

    Added checkValue(), checkIsStrikeOrSpare(), setValueFrameAndPrevFrames(), but need implemantation

--- inc/Game.hpp
+++ inc/Game.hpp
@@ -10,4 +10,7 @@ public:
     std::string getName() const;
     void setName(const std::string &);
     int score() const;
+    int checkValue(const std::string & value);
+    void checkIsStrikeOrSpare(int pos, const std::string & value);
+    void setValueFrameAndPrevFrames(int pos, const std::string & value);
 };
