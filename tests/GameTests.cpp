eb5f20adbc7960cf80d53640768d9c415262510a
t0m3k52 <tomek.szpytma@gmail.com>
2018-09-07 21:48:43 +0200

    added construcor to Game class with string of one game on input

--- tests/GameTests.cpp
+++ tests/GameTests.cpp
@@ -6,22 +6,6 @@ struct GameTests : public ::testing::Test
 
 };
 
-TEST_F(GameTests, name_should_be_empty_to_default_constructor)
-{
-    //GIVEN
-    Game game;
-    //THEN
-    ASSERT_EQ("", game.getName());
-}
-
-TEST_F(GameTests, constructor_with_name_Adam)
-{
-    //GIVEN
-    Game game("Adam");
-    //THEN
-    ASSERT_EQ("Adam", game.getName());
-}
-
 TEST_F(GameTests, set_Is_Strike_Or_Spare)
 {
     //GIVEN
