717bb9071f40bae89e4051a7d8bcd87eacdb2652
t0m3k52 <tomek.szpytma@gmail.com>
2018-09-08 13:15:47 +0200

    added unit tests

--- tests/GameConstructorTests.cpp
+++ tests/GameConstructorTests.cpp
@@ -52,7 +52,7 @@ TEST_F(GameConstructorTests,  NameShouldBeAdamAndScoreShouldBe300)
     ASSERT_EQ(300, score);
 }
 
-TEST_F(GameConstructorTests,  NameShouldBeiMaxAndScoreShouldBe90)
+TEST_F(GameConstructorTests,  NameShouldBeMaxAndScoreShouldBe90)
 {
     //GIVEN
     Game game("Max:9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
@@ -61,4 +61,59 @@ TEST_F(GameConstructorTests,  NameShouldBeiMaxAndScoreShouldBe90)
     //THEN
     ASSERT_EQ("Max", game.getName());
     ASSERT_EQ(90, score);
+}
+
+TEST_F(GameConstructorTests,  NameShouldBeEmptyAndScoreShouldBe150)
+{
+    //GIVEN
+    Game game(":5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5");
+    //WHEN
+    score = game.score();
+    //THEN
+    ASSERT_EQ("", game.getName());
+    ASSERT_EQ(150, score);
+}
+
+TEST_F(GameConstructorTests,  NameShouldBeEmptyAndScoreShouldBe167)
+{
+    //GIVEN
+    Game game(":X|7/|9-|X|-8|8/|-6|X|X|X||81");
+    //WHEN
+    score = game.score();
+    //THEN
+    ASSERT_EQ("", game.getName());
+    ASSERT_EQ(167, score);
+}
+
+TEST_F(GameConstructorTests,  NameShouldBeName1AndScoreShouldBe21)
+{
+    //GIVEN
+    Game game("Name1:X|4-|3");
+    //WHEN
+    score = game.score();
+    //THEN
+    ASSERT_EQ("Name1", game.getName());
+    ASSERT_EQ(21, score);
+}
+
+TEST_F(GameConstructorTests,  NameShouldBeName2AndScoreShouldBe17)
+{
+    //GIVEN
+    Game game("Name2:34|X|0-");
+    //WHEN
+    score = game.score();
+    //THEN
+    ASSERT_EQ("Name2", game.getName());
+    ASSERT_EQ(17, score);
+}
+
+TEST_F(GameConstructorTests,  NameShouldBeEmptyAndScoreShouldBe24)
+{
+    //GIVEN
+    Game game(":X|22|33");
+    //WHEN
+    score = game.score();
+    //THEN
+    ASSERT_EQ("", game.getName());
+    ASSERT_EQ(24, score);
 }
