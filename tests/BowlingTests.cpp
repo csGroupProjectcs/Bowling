cffd2825393e0dcdc2afa942ae7254ad287ff04e
[szymon] <[szymon.janas.95@gmail.com]>
2018-09-11 09:49:33 +0200

    tests for many lanes and games added for toString()

--- tests/BowlingTests.cpp
+++ tests/BowlingTests.cpp
@@ -96,3 +96,75 @@ TEST_F(BowlingTests, toString_compare_empty_lane_and_shown_data_expect_return_tr
     //THEN
     ASSERT_EQ(data, dataFromMethod);
 }
+
+TEST_F(BowlingTests, toString_compare_many_empty_lanes_and_shown_data_expect_return_true)
+{
+    //GIVEN
+    Lane lane1;
+    Lane lane2;
+    Lane lane3;
+    bowling.addLane(lane1);
+    bowling.addLane(lane2);
+    bowling.addLane(lane3);
+    //WHEN
+    std::string data = "### Lane 1: GAME STATUS ###\n### Lane 2: GAME STATUS ###\n### Lane 3: GAME STATUS ###\n";
+    std::string dataFromMethod = bowling.toString();
+    //THEN
+    ASSERT_EQ(data, dataFromMethod);
+}
+
+TEST_F(BowlingTests, toString_compare_many_games_and_shown_data_expect_return_true)
+{
+    //GIVEN
+    Lane lane1;
+    Game game1("Name1:7|9|X|X|6/|X|3|6|7|8");
+    Game game2("Name2:7|9|X|X|6/|9|3|6|7|8");
+    Game game3("Name3:7|X|X|X|6/|8|3|6|X|8");
+    lane1.addGame(game1);
+    lane1.addGame(game2);
+    lane1.addGame(game3);
+    bowling.addLane(lane1);
+
+    //WHEN
+    std::string data = "### Lane 1: GAME STATUS ###\nName1 119\nName2 114\nName3 144\n";
+    std::string dataFromMethod = bowling.toString();
+    //THEN
+    ASSERT_EQ(data, dataFromMethod);
+
+}
+
+TEST_F(BowlingTests, toString_compare_given_data_to_many_lanes_and_many_games_and_shown_data_expect_return_true)
+{
+    //GIVEN
+    Lane lane1;
+    Game game1("Name1:7|9|X|X|6/|3|3|6|7|8");
+    Game game2("Name2:7|9|X|X|6/|9|3|6|7|8");
+    Game game3("Name3:7|X|X|X|7|8|3|6|X|8");
+    lane1.addGame(game1);
+    lane1.addGame(game2);
+    lane1.addGame(game3);
+    bowling.addLane(lane1);
+    Lane lane2;
+    Game game4("Name1:7|9|X|X|5|X|3|6|7|8");
+    Game game5("Name2:7|9|X|X|6/|9|3|6|7|8");
+    Game game6("Name3:7|X|X|X|1|8|3|6|X|8");
+    lane2.addGame(game4);
+    lane2.addGame(game5);
+    lane2.addGame(game6);
+    bowling.addLane(lane2);
+    Lane lane3;
+    Game game7("Name1:7|9|X|X|6/|X|X|6|7|8");
+    Game game8("Name2:7|9|X|X|8|9|3|6|7|8");
+    Game game9("Name3:7|X|X|X|6/|8|3|6|X|8");
+    lane3.addGame(game7);
+    lane3.addGame(game8);
+    lane3.addGame(game9);
+    bowling.addLane(lane3);
+
+    //WHEN
+    std::string data = "### Lane 1: GAME STATUS ###\nName1 102\nName2 114\nName3 131\n### Lane 2: GAME STATUS ###\nName1 102\nName2 114\nName3 131\n### Lane 3: GAME STATUS ###\nName1 102\nName2 114\nName3 131\n";
+    std::string dataFromMethod = bowling.toString();
+    //THEN
+    ASSERT_EQ(data, dataFromMethod);
+
+}
