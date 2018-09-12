9d1ad7fe20679db957c922a6b617d39c4add062e
Andrzej <ganczarek.a@gmil.com>
2018-09-12 22:32:23 +0200

    Added test for read directory and print result on stdout

--- tests/specificBehaviorTests.cpp
+++ tests/specificBehaviorTests.cpp
@@ -22,3 +22,12 @@ TEST_F(specificBehaviorTests, given_dash_dash_help_expect_info_help_message)
     std::string output = testing::internal::GetCapturedStdout();
     ASSERT_EQ("This is program for score bowling \nenter ./Bowling nameDirectoryToread to print result to screen \nor ./Bowling nameDirectoryToread nameFileToSave\n", output);
 }
+
+TEST_F(specificBehaviorTests, given_path_todirectory_expect_information_about_games)
+{
+    char* input[] = {(char*)"./Bowling", (char*)"../tests/data_tests"};
+    testing::internal::CaptureStdout();
+    specificBehavior(2, input);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ("### Lane 1: GAME STATUS ###\nHenryk 24\n### Lane 2: GAME STATUS ###\nKevin 16\nSandra 16\n### Lane 3: GAME STATUS ###\n", output);
+}
