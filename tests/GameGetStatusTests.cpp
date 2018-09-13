bbdb9531eb9de2b91ef6576ee42e0472c4577f25
t0m3k52 <tomek.szpytma@gmail.com>
2018-09-13 12:13:34 +0200

    added tests for getStatusGame method

---
+++ tests/GameGetStatusTests.cpp
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+#include "Game.hpp"
+
+struct GetStatusGameTests : public ::testing::Test
+{
+    std::string status;
+};
+
+TEST_F (GetStatusGameTests, GivenUnfinishedGameWithCompleteFrameAtEndShouldHaveStatusGameInProgress)
+{
+    //GIVEN
+    Game game(":12|21|42");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game in progress", status);
+}
+
+TEST_F (GetStatusGameTests, GivenUnfinishedGameWithIncompleteFrameAtEndShouldHaveStatusGameInProgress)
+{
+    //GIVEN
+    Game game(":12|21|4");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game in progress", status);
+}
+
+TEST_F (GetStatusGameTests, GivenFinishedGameShouldHaveStatusGameFinished)
+{
+    //GIVEN
+    Game game(":12|21|4/|12|X|41|11|-4|--|32||");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game finished", status);
+}
+
+TEST_F (GetStatusGameTests, GivenUnfinishedGameWithSpareIn10FrameShouldHaveStatusGameInProgress)
+{
+    //GIVEN
+    Game game(":12|21|4/|12|X|41|11|-4|--|3/||");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game in progress", status);
+}
+
+TEST_F (GetStatusGameTests, GivenUnfinishedGameWithStrikeIn10FrameAnd0BonusBallsShouldHaveStatusGameInProgress)
+{
+    //GIVEN
+    Game game(":12|21|4/|12|X|41|11|-4|--|X||");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game in progress", status);
+}
+
+TEST_F (GetStatusGameTests, GivenUnfinishedGameWithStrikeIn10FrameAnd1BonusBallsShouldHaveStatusGameInProgress)
+{
+    //GIVEN
+    Game game(":12|21|4/|12|X|41|11|-4|--|X||5");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game in progress", status);
+}
+
+TEST_F (GetStatusGameTests, GivenFinishedGameWithStrikeIn10FrameShouldHaveStatusGameFinished)
+{
+    //GIVEN
+    Game game(":12|21|4/|12|X|41|11|-4|--|X||52");
+    //WHEN
+    status = game.getStatusGame();
+    //THEN
+    ASSERT_EQ("game finished", status);
+}
